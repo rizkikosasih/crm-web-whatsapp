0637dbfa7a65e2847b3e2a3769fa852589664cf3
Rizki <rizkikosasih1997@gmail.com>
2025-05-24 12:41:25 +0700

    message table add column user_id

--- database/migrations/2025_05_17_020059_create_orders_table.php
+++ database/migrations/2025_05_17_020059_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
     Schema::create('orders', function (Blueprint $table) {
       $table->id();
       $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+      $table->foreignId('user_id')->constrained()->onDelete('cascade');
       $table->date('order_date');
       $table->decimal('total_amount', 12, 2)->default(0);
       $table->tinyInteger('status')->default(0);
