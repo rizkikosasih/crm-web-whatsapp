134c744564c5945ebe23eba754cf7ac28de502f8
Rizki <rizkikosasih1997@gmail.com>
2025-05-24 19:20:51 +0700

    add livewire listener for success alert and scroll to top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,12 +2,16 @@
 
 <script type="module">
   Livewire.on('showError', function (e) {
-    Toast.fire({icon: 'error', title: e[0].message});
+    Toast.fire({icon: 'error', title: e.message});
   });
 
-  Livewire.on('test', function (e) {
-    console.info(e);
-  })
+  Livewire.on('showSuccess', function (e) {
+    Toast.fire({icon: 'success', title: e.message});
+  });
+
+  Livewire.on('scrollToTop', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 </script>
 
 @yield('page-script')
