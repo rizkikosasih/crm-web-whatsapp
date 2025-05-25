a99da1fe6e00bcc6f5f7a441fd73022b228e4ae8
Febryana <febryana0219@gmail.com>
2025-05-25 21:12:58 +0700

    add seeder wa api

---
+++ database/seeders/WhatsappSettingsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class WhatsappSettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('whatsapp_settings')->insert([
+            'key' => '-',
+            'url' => '-',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
