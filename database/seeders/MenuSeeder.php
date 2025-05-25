ac55b2f8a613808d2412b359591aaa026cd1e0b6
Rizki <rizkikosasih1997@gmail.com>
2025-05-25 21:59:32 +0700

    sederhanakan menu seeder agar mudah diupdate

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -17,188 +17,187 @@ class MenuSeeder extends Seeder
   {
     DB::beginTransaction();
 
-    //Head Menu
-    $menu = Menu::create([
-      'name' => '',
-      'position' => 1,
-    ]);
+    // Roles
+    $roles = [
+      'super-admin' => Role::create(['name' => 'Super Admin']),
+      'admin' => Role::create(['name' => 'Admin']),
+    ];
 
-    $master = Menu::create([
-      'name' => 'Master',
-      'position' => 2,
-    ]);
+    // Header Menus
+    $headers = [
+      '' => 1,
+      'Master' => 2,
+      'Transaksi' => 3,
+      'Laporan' => 4,
+      'Pengaturan' => 5,
+    ];
 
-    $transaksi = Menu::create([
-      'name' => 'Transaksi',
-      'position' => 3,
-    ]);
+    $headerIds = [];
+    foreach ($headers as $name => $position) {
+      $headerIds[$name] = Menu::create([
+        'name' => $name,
+        'position' => $position,
+      ]);
+    }
 
-    $laporan = Menu::create([
-      'name' => 'Laporan',
-      'position' => 4,
-    ]);
+    // Child Menus
+    $menus = [
+      [
+        'name' => 'Dashboard',
+        'icon' => 'fas fa-dashboard',
+        'route' => '/dashboard',
+        'slug' => 'dashboard',
+        'position' => 1,
+        'parent' => '',
+        'roles' => ['super-admin', 'admin'],
+      ],
+      [
+        'name' => 'Pelanggan',
+        'icon' => 'fas fa-address-book',
+        'route' => '/customer',
+        'slug' => 'customer',
+        'position' => 1,
+        'parent' => 'Master',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Produk',
+        'icon' => 'fas fa-box',
+        'route' => '/product',
+        'slug' => 'product',
+        'position' => 2,
+        'parent' => 'Master',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Campaign Broadcast',
+        'icon' => 'fas fa-bullhorn',
+        'route' => '/campaign',
+        'slug' => 'campaign',
+        'position' => 3,
+        'parent' => 'Master',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Pesanan',
+        'icon' => 'fas fa-shop',
+        'route' => '/order',
+        'slug' => 'order',
+        'position' => 1,
+        'parent' => 'Transaksi',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Pesan Keluar',
+        'icon' => 'fas fa-envelope-open',
+        'route' => '/message/out',
+        'slug' => 'message-out',
+        'position' => 2,
+        'parent' => 'Transaksi',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Penjualan',
+        'icon' => 'fas fa-chart-simple',
+        'route' => '/report/order',
+        'slug' => 'report-order',
+        'position' => 1,
+        'parent' => 'Laporan',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Penjualan Per Produk',
+        'icon' => 'fas fa-table-columns',
+        'route' => '/report/product',
+        'slug' => 'report-product',
+        'position' => 2,
+        'parent' => 'Laporan',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Template Pesan',
+        'icon' => 'fas fa-comment',
+        'route' => '/setting/template',
+        'slug' => 'setting-template',
+        'position' => 1,
+        'parent' => 'Pengaturan',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'User',
+        'icon' => 'fas fa-users',
+        'route' => '/setting/user',
+        'slug' => 'setting-user',
+        'position' => 2,
+        'parent' => 'Pengaturan',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Role',
+        'icon' => 'fas fa-wrench',
+        'route' => '/setting/role',
+        'slug' => 'setting-role',
+        'position' => 3,
+        'parent' => 'Pengaturan',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Menu',
+        'icon' => 'fas fa-bars',
+        'route' => '/setting/menu',
+        'slug' => 'setting-menu',
+        'position' => 4,
+        'parent' => 'Pengaturan',
+        'roles' => ['super-admin'],
+      ],
+      [
+        'name' => 'Whatsapp Api',
+        'icon' => 'fab fa-whatsapp',
+        'route' => '/setting/whatsapp-api',
+        'slug' => 'setting-whatsapp-api',
+        'position' => 5,
+        'parent' => 'Pengaturan',
+        'roles' => ['super-admin'],
+      ],
+    ];
 
-    $setting = Menu::create([
-      'name' => 'Pengaturan',
-      'position' => 5,
-    ]);
+    // Insert child menus and role access
+    $menuRoles = [];
+    foreach ($menus as $menuData) {
+      $menu = Menu::create([
+        'name' => $menuData['name'],
+        'icon' => $menuData['icon'],
+        'route' => $menuData['route'],
+        'slug' => $menuData['slug'],
+        'position' => $menuData['position'],
+        'parent_id' => $headerIds[$menuData['parent']]->id ?? null,
+      ]);
 
-    //Menu
-    $dashboard = Menu::create([
-      'name' => 'Dashboard',
-      'icon' => 'fas fa-dashboard',
-      'route' => '/dashboard',
-      'slug' => 'dashboard',
-      'position' => 1,
-      'parent_id' => $menu->id,
-    ]);
+      foreach ($menuData['roles'] as $roleName) {
+        $menuRoles[] = [
+          'menu_id' => $menu->id,
+          'role_id' => $roles[$roleName]->id,
+        ];
+      }
+    }
 
-    $customer = Menu::create([
-      'name' => 'Pelanggan',
-      'icon' => 'fas fa-address-book',
-      'route' => '/customer',
-      'slug' => 'customer',
-      'position' => 1,
-      'parent_id' => $master->id,
-    ]);
+    // Header Role Access
+    foreach ($headers as $name => $pos) {
+      $menuRoles[] = [
+        'menu_id' => $headerIds[$name]->id,
+        'role_id' => $roles['super-admin']->id,
+      ];
 
-    $product = Menu::create([
-      'name' => 'Produk',
-      'icon' => 'fas fa-box',
-      'route' => '/product',
-      'slug' => 'product',
-      'position' => 2,
-      'parent_id' => $master->id,
-    ]);
+      // Only '' (Dashboard group) is for admin
+      if ($name === '') {
+        $menuRoles[] = [
+          'menu_id' => $headerIds[$name]->id,
+          'role_id' => $roles['admin']->id,
+        ];
+      }
+    }
 
-    $campaign = Menu::create([
-      'name' => 'Campaign Broadcast',
-      'icon' => 'fas fa-bullhorn',
-      'route' => '/campaign',
-      'slug' => 'campaign',
-      'position' => 3,
-      'parent_id' => $master->id,
-    ]);
-
-    $order = Menu::create([
-      'name' => 'Pesanan',
-      'icon' => 'fas fa-shop',
-      'route' => '/order',
-      'slug' => 'order',
-      'position' => 1,
-      'parent_id' => $transaksi->id,
-    ]);
-
-    $messageOut = Menu::create([
-      'name' => 'Pesan Keluar',
-      'icon' => 'fas fa-envelope-open',
-      'route' => '/message/out',
-      'slug' => 'message-out',
-      'position' => 2,
-      'parent_id' => $transaksi->id,
-    ]);
-
-    $penjualan = Menu::create([
-      'name' => 'Penjualan',
-      'icon' => 'fas fa-chart-simple',
-      'route' => '/report/order',
-      'slug' => 'report-order',
-      'position' => 1,
-      'parent_id' => $laporan->id,
-    ]);
-
-    $penjualanPerProduk = Menu::create([
-      'name' => 'Penjualan Per Produk',
-      'icon' => 'fas fa-table-columns',
-      'route' => '/report/product',
-      'slug' => 'report-product',
-      'position' => 2,
-      'parent_id' => $laporan->id,
-    ]);
-
-    $messageTemplate = Menu::create([
-      'name' => 'Template Pesan',
-      'icon' => 'fas fa-comment',
-      'route' => '/message/template',
-      'slug' => 'message-template',
-      'position' => 1,
-      'parent_id' => $setting->id,
-    ]);
-
-    $user = Menu::create([
-      'name' => 'User',
-      'icon' => 'fas fa-users',
-      'route' => '/setting/user',
-      'slug' => 'setting-user',
-      'position' => 2,
-      'parent_id' => $setting->id,
-    ]);
-
-    $role = Menu::create([
-      'name' => 'Role',
-      'icon' => 'fas fa-wrench',
-      'route' => '/setting/role',
-      'slug' => 'setting-role',
-      'position' => 3,
-      'parent_id' => $setting->id,
-    ]);
-
-    $submenu = Menu::create([
-      'name' => 'Menu',
-      'icon' => 'fas fa-bars',
-      'route' => '/setting/menu',
-      'slug' => 'setting-menu',
-      'position' => 4,
-      'parent_id' => $setting->id,
-    ]);
-
-    $submenu = Menu::create([
-      'name' => 'Whatsapp Api',
-      'icon' => 'fab fa-whatsapp',
-      'route' => '/setting/whatsapp-api',
-      'slug' => 'setting-whatsapp-api',
-      'position' => 5,
-      'parent_id' => $setting->id,
-    ]);
-
-    $superAdmin = Role::create(['name' => 'Super Admin']);
-    $admin = Role::create(['name' => 'Admin']);
-
-    MenuRole::insert([
-      /**
-       * Super Admin
-       * Header
-       */
-      ['menu_id' => $menu->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $master->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $transaksi->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $laporan->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $setting->id, 'role_id' => $superAdmin->id],
-
-      /** Menu */
-      ['menu_id' => $dashboard->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $customer->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $product->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $order->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $campaign->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $messageOut->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $penjualan->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $penjualanPerProduk->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $messageTemplate->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $user->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $role->id, 'role_id' => $superAdmin->id],
-      ['menu_id' => $submenu->id, 'role_id' => $superAdmin->id],
-
-      /**
-       * Admin
-       * Header
-       */
-      ['menu_id' => $menu->id, 'role_id' => $admin->id],
-
-      /** Menu */
-      ['menu_id' => $dashboard->id, 'role_id' => $admin->id],
-    ]);
-
+    MenuRole::insert($menuRoles);
     DB::commit();
   }
 }
