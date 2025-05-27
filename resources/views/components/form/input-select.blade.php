47f113adf9de6ffdd9338bcc731ce22458638f6e
Rizki <rizkikosasih1997@gmail.com>
2025-05-27 22:10:43 +0700

    refactor input-select component to enhance structure and improve readability

--- resources/views/components/form/input-select.blade.php
+++ resources/views/components/form/input-select.blade.php
@@ -1,3 +1,49 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+@props([
+  'parentClass' => null,
+  'customClass' => null,
+  'label' => null,
+  'name' => null,
+  'type' => 'text',
+  'horizontal' => false,
+  'options' => [],
+  'optionHeader' => null,
+  'selected' => null,
+])
+
+<div @class(['form-group', 'row' => $horizontal, $parentClass])>
+  @isset($label)
+    <label for="{{ $name }}" class="fw-bold @if($horizontal) col-sm-3 @endif">{!! $label !!}</label>
+  @endisset
+
+  @if($horizontal)
+    <div class="col-sm-9">
+  @endif
+
+    <select
+      name="{{ $name }}"
+      id="{{ $id ?? $name }}"
+      @class([
+        'form-control',
+        $customClass ?? '',
+        'is-invalid' => $errors->has($name),
+      ])
+      {{ $attributes }}
+    >
+      @if($optionHeader)
+        <option value="">{{ $optionHeader }}</option>
+      @endif
+
+      @foreach ($options as $key => $value)
+        <option value="{{ $key }}" {{ $selected == $key ? 'selected' : '' }}>{!! $value !!}</option>
+      @endforeach
+    </select>
+
+    @error($name)
+      <x-alert.danger customClass="mt-2">{{ $message }}</x-alert.danger>
+    @enderror
+
+  @if($horizontal)
+    </div>
+  @endif
+
 </div>
