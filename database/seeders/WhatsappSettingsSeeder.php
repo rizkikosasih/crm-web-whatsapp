2f64eb5a557266823e4b8181d2e3321e89cc6ae6
Rizki <rizkikosasih1997@gmail.com>
2025-05-27 19:35:10 +0700

    update url whatsapp api

--- database/seeders/WhatsappSettingsSeeder.php
+++ database/seeders/WhatsappSettingsSeeder.php
@@ -7,16 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class WhatsappSettingsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        DB::table('whatsapp_settings')->insert([
-            'key' => '-',
-            'url' => '-',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    DB::table('whatsapp_settings')->insert([
+      'key' => '-',
+      'url' => 'https://panel.rapiwha.com/send_message.php',
+      'created_at' => now(),
+      'updated_at' => now(),
+    ]);
+  }
 }
