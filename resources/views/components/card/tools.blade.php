ca0f6340d8198aa7c7ccab27d7e08e37e3df4581
Rizki <rizkikosasih1997@gmail.com>
2025-05-31 10:35:35 +0700

    feat: add URL link to card tools component for navigation

--- resources/views/components/card/tools.blade.php
+++ resources/views/components/card/tools.blade.php
@@ -1,9 +1,16 @@
 @props([
   'refresh' => null,
   'minus' => null,
+  'url' => null,
 ])
 
 <div class="card-tools">
+  @if ($url)
+    <a href="{{ $url }}" class="btn btn-tool tooltips" title="Kembali" title wire:navigate>
+      <i class="fas fa-backward-step"></i>
+    </a>
+  @endif
+
   @if ($refresh)
     <button type="button" class="btn btn-tool" wire:click="$refresh">
       <i class="fas fa-refresh"></i>
