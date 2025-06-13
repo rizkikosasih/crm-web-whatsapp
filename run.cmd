45811b5f0d3eb08fab12b8281bebad94d57818a1
Rizki <rizkikosasih1997@gmail.com>
2025-06-13 21:51:59 +0700

    fix: reorder browser launch and server start for improved script flow

--- run.cmd
+++ run.cmd
@@ -2,9 +2,9 @@
 title Laravel + Vite Starter
 cd /d %~dp0
 
+echo Membuka browser...
+start "" http://localhost:8000
+
 echo Menjalankan Laravel + Vite Dev Server...
 npm run dev-all
 yarn dev-all
-
-echo Membuka browser...
-start "" http://localhost:8000
