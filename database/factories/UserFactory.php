ad2c767e6d10083f0c1fdbfea9fe0a0cb0701471
Rizki <rizkikosasih1997@gmail.com>
2025-06-15 03:26:40 +0700

    refactor: update UserFactory to ensure address is always generated and reset last_login_at to null

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,9 @@ class UserFactory extends Factory
       'phone' => $this->faker->optional()->numerify('628############'),
       'email_verified_at' => now(),
       'role_id' => Role::inRandomOrder()->first()?->id ?? Role::factory(),
-      'address' => $this->faker->optional()->address(),
+      'address' => $this->faker->address(),
       'avatar' => null, // Atau generate URL dummy pakai faker()->imageUrl() jika perlu
-      'last_login_at' => $this->faker->optional()->dateTimeBetween('-1 month'),
+      'last_login_at' => null,
       'last_login_ip' => null,
       'is_active' => true,
     ];
