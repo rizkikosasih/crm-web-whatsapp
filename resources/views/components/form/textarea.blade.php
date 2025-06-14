7965e0ab093ebc9092e392ff5edbc82ee7abef78
Rizki <rizkikosasih1997@gmail.com>
2025-06-14 22:02:11 +0700

    refactor: enhance textarea component structure for better layout and styling

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,18 +1,32 @@
-<div @class(['form-group', $parentClass ?? ''])>
+@props([
+  'parentClass' => null,
+  'customClass' => null,
+  'label' => null,
+  'name' => null,
+  'horizontal' => false,
+])
+
+<div @class(['form-group', 'row' => $horizontal, $parentClass])>
   @isset($label)
-    <label for="{{ $name }}" class="fw-bold">{!! $label !!}</label>
+    <label for="{{ $name }}" class="fw-bold @if($horizontal) col-sm-3 @endif">{!! $label !!}</label>
   @endisset
 
-  <textarea
-    @class([
-      'form-control',
-      $customClass ?? '',
-      'is-invalid' => $errors->has($name),
-    ])
-    {{ $attributes }}
-  ></textarea>
+  @if ($horizontal)
+    <div class="col-sm-9">
+  @endif
 
-  @error('alamat')
-    <x-alert.text-danger customClass="mt-2">{{ $message }}</x-alert.text-danger>
-  @enderror
+    <textarea
+      @class([
+        'form-control',
+        'is-invalid' => $errors->has($name),
+        $customClass,
+      ])
+      {{ $attributes }}
+    ></textarea>
+
+    @error($name)
+      <x-alert.text-danger customClass="mt-2">{{ $message }}</x-alert.text-danger>
+    @enderror
+
+  </div>
 </div>
