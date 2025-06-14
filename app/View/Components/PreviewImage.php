18dcc3f632c5f22a38c40213828d9db9394210f6
Rizki <rizkikosasih1997@gmail.com>
2025-06-15 01:02:58 +0700

    refactor: standard laravel component

--- app/View/Components/PreviewImage.php
+++ app/View/Components/PreviewImage.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class overlay extends Component
+class PreviewImage extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class overlay extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.overlay');
+        return view('components.preview-image');
     }
 }
--- app/View/Components/SidebarMenu.php
+++ app/View/Components/SidebarMenu.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class button extends Component
+class SidebarMenu extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class button extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.button');
+        return view('components.sidebar-menu');
     }
 }
