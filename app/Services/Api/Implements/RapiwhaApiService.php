1eb48f484d37a0ae0fea6ebb98e0fb12cdb4d698
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-04-29 16:44:21 +0700

    feat: update sendMessage method to accept an optional imageUrl parameter

--- app/Services/Api/Implements/RapiwhaApiService.php
+++ app/Services/Api/Implements/RapiwhaApiService.php
@@ -11,97 +11,105 @@ use Illuminate\Support\Facades\Http;
 
 class RapiwhaApiService implements SendMessageApiServiceInterface
 {
-  protected string $baseUrl, $apiKey;
+    protected string $baseUrl, $apiKey;
 
-  public function __construct()
-  {
-    $this->apiKey = waApiKey();
-    $this->baseUrl = waApiUrl();
-  }
-
-  public function sendMessage(
-    string $number,
-    string $text,
-    bool $isObject = true
-  ): array|object {
-    try {
-      DB::beginTransaction();
-
-      $customer = Customer::where('phone', $number)->first();
-      if (!$customer) {
-        return $this->formatResponse(
-          false,
-          'Nomor Handphone tidak ditemukan dalam daftar pelanggan',
-          $isObject
-        );
-      }
-
-      /* For Testing */
-      Http::fake([
-        'rapiwha.com/*' => Http::response(
-          [
-            'success' => true,
-            'description' => 'Message queued',
-            'result_code' => 0,
-          ],
-          200
-        ),
-      ]);
-
-      // Kirim pesan utama (hanya 1x)
-      $response = Http::timeout(10)
-        ->retry(3, 1000)
-        ->get($this->baseUrl, [
-          'apikey' => $this->apiKey,
-          'number' => $number,
-          'text' => $text,
-        ]);
-
-      if ($response->successful()) {
-        $result = $response->json();
-
-        if (isset($result['result_code']) && $result['result_code'] === 0) {
-          // Simpan ke DB
-          Message::create([
-            'customer_id' => $customer->id,
-            'user_id' => Auth::id(),
-            'message' => e($text),
-            'image' => null,
-          ]);
-        }
-
-        DB::commit();
-        return $this->formatResponse(
-          $result['success'],
-          $result['description'],
-          $isObject
-        );
-      }
-
-      return $this->formatResponse(
-        false,
-        $response->status() . ': ' . $response->body(),
-        $isObject
-      );
-    } catch (\Exception $e) {
-      DB::rollBack();
-      logger('HTTP Error: ' . $e->getMessage());
-      return $this->formatResponse(false, $e->getMessage(), $isObject);
+    public function __construct()
+    {
+        $this->apiKey = waApiKey();
+        $this->baseUrl = waApiUrl();
     }
-  }
 
-  protected function formatResponse(
-    bool $success,
-    string $message,
-    bool $asObject = true,
-    array $data = []
-  ): array|object {
-    $result = [
-      'success' => $success,
-      'message' => $message,
-      'data' => $data,
-    ];
+    public function sendMessage(
+        string $number,
+        string $text,
+        ?string $fileUrl = null,
+        bool $isObject = true
+    ): array|object {
+        try {
+            DB::beginTransaction();
 
-    return $asObject ? (object) $result : $result;
-  }
+            $customer = Customer::where('phone', $number)->first();
+            if (!$customer) {
+                return $this->formatResponse(
+                    false,
+                    'Nomor Handphone tidak ditemukan dalam daftar pelanggan',
+                    $isObject
+                );
+            }
+
+            /* For Testing
+            Http::fake([
+                'rapiwha.com/*' => Http::response(
+                [
+                    'success' => true,
+                    'description' => 'Message queued',
+                    'result_code' => 0,
+                ],
+                200
+                ),
+            ]); */
+
+            // Kirim gambar terlebih dahulu jika ada
+            if (filled($fileUrl)) {
+                Http::timeout(10)
+                    ->retry(3, 1000)
+                    ->get($this->baseUrl, [
+                        'apikey' => $this->apiKey,
+                        'number' => $number,
+                        'text' => $fileUrl,
+                    ]);
+            }
+
+            // Kirim pesan utama (hanya 1x)
+            $response = Http::timeout(10)
+                ->retry(3, 1000)
+                ->get($this->baseUrl, [
+                    'apikey' => $this->apiKey,
+                    'number' => $number,
+                    'text' => $text,
+                ]);
+
+            if ($response->successful()) {
+                $result = $response->json();
+
+                if (isset($result['result_code']) && $result['result_code'] === 0) {
+                    // Simpan ke DB
+                    Message::create([
+                        'customer_id' => $customer->id,
+                        'user_id' => Auth::id(),
+                        'message' => e($text),
+                        'image' => $fileUrl,
+                    ]);
+                }
+
+                DB::commit();
+                return $this->formatResponse($result['success'], $result['description'], $isObject);
+            }
+
+            return $this->formatResponse(
+                false,
+                $response->status() . ': ' . $response->body(),
+                $isObject
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+            logger('HTTP Error: ' . $e->getMessage());
+            return $this->formatResponse(false, $e->getMessage(), $isObject);
+        }
+    }
+
+    protected function formatResponse(
+        bool $success,
+        string $message,
+        bool $asObject = true,
+        array $data = []
+    ): array|object {
+        $result = [
+            'success' => $success,
+            'message' => $message,
+            'data' => $data,
+        ];
+
+        return $asObject ? (object) $result : $result;
+    }
 }
--- app/Services/Api/SendMessageApiServiceInterface.php
+++ app/Services/Api/SendMessageApiServiceInterface.php
@@ -7,6 +7,7 @@ interface SendMessageApiServiceInterface
   public function sendMessage(
     string $number,
     string $text,
+    ?string $imageUrl = null,
     bool $isObject = true
   ): array|object;
 }
