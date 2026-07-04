2f09124f4e7c9b74f90ca3947372e40281d094d0
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-07-05 03:43:35 +0700

    chore: remove temporary connection test script

--- test_conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-use Illuminate\Contracts\Console\Kernel;
-$app->make(Kernel::class)->bootstrap();
-
-Log::info('TEST LOG FROM PHP FILE');
-echo "Log written!\n";
